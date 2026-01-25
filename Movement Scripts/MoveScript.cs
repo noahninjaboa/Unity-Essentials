6944570786cb6a1cf1ab0fe28c5b4b5b71152f43
Noah <noahninjaboa@gmail.com>
2026-01-24 17:20:12 -0800

    Added Movement Scripts, README

---
+++ Movement Scripts/MoveScript.cs
@@ -0,0 +1,56 @@
+using UnityEngine;          //this is a simple script with movement, running, and flipping when changing directions
+using UnityEngine.InputSystem;
+using System;
+public class MoveScript : MonoBehaviour
+{
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float moveDirection = 0;
+    [SerializeField] float runSpeed = 15f;
+    public float geckoDirection = 1;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }    
+    void Start()
+    { 
+
+    }
+
+
+    void Update()
+    {
+        speed = 5f;
+        moveDirection = 0f;
+        
+        if (Keyboard.current.shiftKey.isPressed)
+        {
+            speed = runSpeed;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            moveDirection = 1f;
+            if (transform.localScale != new Vector3(1, 1, 1))
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                OnTurn?.Invoke();
+                geckoDirection = 1;
+                
+            }
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            moveDirection = -1f;
+            transform.localScale = new Vector3(-1, 1, 1);
+            OnTurn?.Invoke();
+            geckoDirection = -1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveDirection * speed, rb.linearVelocity.y);
+    }
+}
